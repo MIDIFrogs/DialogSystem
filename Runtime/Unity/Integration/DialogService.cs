0e08aa93c3b48bc9fbb6610cdf2819e9caf73c6d
Ivan Olyanishin <56886020+IOExcept10n@users.noreply.github.com>
2026-04-28 00:41:35 +0500

    Updated dialog service to set context at runtime

--- Runtime/Unity/Integration/DialogService.cs
+++ Runtime/Unity/Integration/DialogService.cs
@@ -5,10 +5,10 @@ using MIDIFrogs.DialogSystem.Unity.View;
 
 namespace MIDIFrogs.DialogSystem.Unity.Integration
 {
-    public class DialogService
+    public class DialogService(IDialogView view)
     {
-        private readonly IDialogView view;
-        private readonly IDialogContext context;
+        private readonly IDialogView view = view;
+        private IDialogContext context = new DialogContext();
 
         private CancellationTokenSource flowControlSource;
 
@@ -18,11 +18,12 @@ namespace MIDIFrogs.DialogSystem.Unity.Integration
 
         public bool IsPlaying => currentRunner != null;
 
-        public DialogService(IDialogView view, IDialogContext context)
+        public DialogService(IDialogView view, IDialogContext context) : this(view)
         {
-            this.view = view;
             this.context = context;
         }
+
+        public void SetContext(IDialogContext context) => this.context = context;
 
         public async UniTask Play(Dialog dialog, CancellationToken token = default)
         {
